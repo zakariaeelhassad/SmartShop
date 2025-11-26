42ceb29872927cc9b6d04a8bd426eff78203f7a9
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-11-26 12:06:10 +0100

    Add remaining project files including SmartShopApplication and resources

---
+++ src/main/java/org/example/smartshop/SmartShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.smartshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartShopApplication.class, args);
+    }
+
+}
