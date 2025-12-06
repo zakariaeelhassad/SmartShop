65547568fc569450c08e2bff99d08a45efc70140
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-12-06 16:03:31 +0100

    Mise à jour de l'interface du service Produit

--- src/main/java/org/example/smartshop/services/IProductService.java
+++ src/main/java/org/example/smartshop/services/IProductService.java
@@ -1,4 +1,16 @@
 package org.example.smartshop.services;
 
+import org.example.smartshop.entities.DTO.product.ProductCreateDto;
+import org.example.smartshop.entities.DTO.product.ProductResponseDto;
+import org.example.smartshop.entities.DTO.product.ProductUpdateDto;
+
+import java.util.List;
+
 public interface IProductService {
+
+    ProductResponseDto createProduct(ProductCreateDto dto);
+    ProductResponseDto updateProduct(ProductUpdateDto dto);
+    void deleteProduct(Long id);
+    List<ProductResponseDto> getAllProducts();
+    ProductResponseDto getProductById(Long id);
 }
