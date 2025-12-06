136acd0067ebfe7acb7e6d482e7312374b97d1b7
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-12-06 16:02:59 +0100

    Amélioration de l'exception ressource non trouvée

--- src/main/java/org/example/smartshop/exceptions/ResourceNotFoundException.java
+++ src/main/java/org/example/smartshop/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,20 @@
 package org.example.smartshop.exceptions;
 
-public class ResourceNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s non trouvé avec %s : '%s'", resourceName, fieldName, fieldValue));
+    }
 }
