7ed1997ac8e3f87e2ba1850b65b9a071822774e8
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-12-06 16:02:56 +0100

    Amélioration du format des réponses d’erreur

--- src/main/java/org/example/smartshop/exceptions/ErrorResponse.java
+++ src/main/java/org/example/smartshop/exceptions/ErrorResponse.java
@@ -1,4 +1,22 @@
 package org.example.smartshop.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private Map<String, String> validationErrors;
 }
