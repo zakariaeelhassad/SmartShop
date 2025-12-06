dae4d777fb57d05fab6aea020d6770a447c4795b
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-12-06 16:02:52 +0100

    Amélioration de l'exception métier

--- src/main/java/org/example/smartshop/exceptions/BusinessException.java
+++ src/main/java/org/example/smartshop/exceptions/BusinessException.java
@@ -1,4 +1,16 @@
 package org.example.smartshop.exceptions;
 
-public class BusinessException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
